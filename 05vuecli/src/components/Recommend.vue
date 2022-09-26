ea8d63d946bce1cebf26eb2258c8e8e1e38dd417
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-26 11:51:55 +0900

    レコメンド機能

--- 05vuecli/src/components/Recommend.vue
+++ 05vuecli/src/components/Recommend.vue
@@ -52,7 +52,7 @@
       </div>
       <!---------- 結果 ---------->
       <div class="qbox" v-if="status === 8">
-        <h2>あなたにぴったりなのは…</h2>
+        <h2>あなたに<br class="pcoff">ぴったりなのは…</h2>
         <img v-bind:src="image" />
         <h3>{{ name }}</h3>
         <h4>￥{{ price }}</h4>
@@ -66,10 +66,157 @@
 <script>
 export default {
   name: "Recommend",
+  data: () => {
+    return {
+      ans1: '',
+      ans2: '',
+      ans3: '',
+      image: '',
+      name: '',
+      price:'',
+      status: 1,
+      menu: {
+        vasty: {
+          western: {
+            cheese: {
+              image: 'images/lunch1.jpg',
+              name: 'Wチーズのピザランチ',
+              price: '880'
+            },
+            tomato: {
+              image: 'images/lunch2.jpg',
+              name: 'トマトとチーズのピザランチ',
+              price: '880'
+            },
+            chicken: {
+              image: 'images/food3.jpg',
+              name: '焼きリゾット',
+              price: '850'
+            }
+          },
+          japanese: {
+            gem: {
+              image: 'images/food2.jpg',
+              name: '自家製ドライカレー',
+              price: '900'
+            },
+            vege: {
+              image: 'images/food1.jpg',
+              name: '自家製スープカレー',
+              price: '900'
+            },
+            old: {
+              image: 'images/food4.jpg',
+              name: 'ナポリタン',
+              price: '750'
+            }
+          }
+        },
+        relax: {
+          cake: {
+            rich: {
+              image: 'images/sweet1.jpg',
+              name: 'チーズケーキ',
+              price: '500'
+            },
+            bitter: {
+              image: 'images/sweet2.jpg',
+              name: 'フォンダショコラ',
+              price: '500'
+            },
+            fragrant: {
+              image: 'images/sweet3.jpg',
+              name: 'ダージリンとハーブのケーキ',
+              price: '480'
+            },
+            banana: {
+              image: 'images/sweet4.jpg',
+              name: 'バナナケーキ',
+              price: '450'
+            }
+          },
+          cafe: {
+            coffee: {
+              image: 'images/drink1.jpg',
+              name: 'コーヒー',
+              price: '380'
+            },
+            snazzy: {
+              image: 'images/drink2.jpg',
+              name: 'ラベンダーティー',
+              price: '530'
+            },
+            discerning: {
+              image: 'images/drink3.jpg',
+              name: '手作りジンジャエール',
+              price: '480'
+            },
+            mild: {
+              image: 'images/drink4.jpg',
+              name: 'カフェラテ',
+              price: '520'
+            }
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    // 今の気分は？
+    q1: function (ans) {
+      this.ans1 = ans;
+      if (ans === 'vasty') {
+        this.status = 2;
+      } else if (ans === 'relax') {
+        this.status = 3;
+      }
+    },
+    // 何が食べたい？
+    q2: function (ans) {
+      this.ans2 = ans;
+      if (ans === 'western') {
+        this.status = 4;
+      } else if (ans === 'japanese') {
+        this.status = 5;
+      }
+    },
+    q3: function (ans) {
+      this.ans2 = ans;
+      if (ans === 'cake') {
+        this.status = 6;
+      } else if (ans === 'cafe') {
+        this.status = 7;
+      }
+    },
+    // 好きな食材（味）は？
+    q4: function (ans) {
+      this.ans3 = ans;
+      this.status = 8;
+      this.image = this.menu[this.ans1][this.ans2][this.ans3].image
+      this.name = this.menu[this.ans1][this.ans2][this.ans3].name
+      this.price = this.menu[this.ans1][this.ans2][this.ans3].price
+    },
+    reset: function () {
+      this.status = 1;
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+h2 {
+  font-size: 16px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  line-height: 1.5;
+  width: 100%;
+}
+
+.pcoff {
+  display: none;
+}
 /*-------------------------
   レコメンド機能
 -------------------------*/
@@ -140,6 +287,10 @@ export default {
   SP
 -------------------------*/
 @media screen and (max-width: 767px) {
+  .pcoff {
+    display: block;
+  }
+
   .recommend .recommendbox p {
     line-height: 25px;
   }
